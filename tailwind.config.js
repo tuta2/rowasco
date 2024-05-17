805382097a15151a55d26bdb17ae7fd789322d9f
tuta2 <reyreghan20@gmail.com>
2024-05-18 00:21:13 +0300

    added preline

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
-  ]
+    './app/javascript/**/*.js',
+    'node_modules/preline/dist/*.js',
+  ],
+  plugins: [
+    require('preline/plugin'),
+  ],
 }
