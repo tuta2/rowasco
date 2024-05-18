c71d7b5346997331afd3791524e3bbd2a97a0a01
tuta2 <reyreghan20@gmail.com>
2024-05-18 11:33:55 +0300

    Added sign up and sign in UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   get 'home/index'
+  resources :sessions, only: [:create]
+  get '/auth/:provider/callback', to: 'sessions#create_auth_user'
+  get 'login', to: 'sessions#new'
+  get "logout", to: "sessions#destroy"
+  get 'signup', to: 'users#new'
+  resources :users, only: [:create, :update, :destroy]
 
   root "home#index"
 end
