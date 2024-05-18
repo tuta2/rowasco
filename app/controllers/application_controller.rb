4132bfe2a7ce0dd79e8b1e1cb0a5d4f67932d49f
tuta2 <reyreghan20@gmail.com>
2024-05-18 14:15:19 +0300

    added user layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,18 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
   helper_method :logged_in?
 
+  layout :layout_by_resource
+
   rescue_from CanCan::AccessDenied do |exception|
     flash[:alert] = "You are not authorized to perform this action❗"
     redirect_back(fallback_location: root_path)
+  end
+
+  def layout_by_resource
+    if logged_in?
+      'user_layout'
+    else
+      'application'
+    end
   end
 end
